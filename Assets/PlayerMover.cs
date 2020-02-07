4b60addd77f4fab3e4f7dc6d14017af5aa02d91d
Ben Mora <blackopsben@gmail.com>
2020-02-07 00:38:57 -0600

    Getting started...

---
+++ Assets/PlayerMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+
+    float movementAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movementAxis = Input.GetAxis("Horizontal");
+        transform.position = transform.position + new Vector3(movementAxis * Time.deltaTime * moveSpeed, 0f, 0f);
+        if (movementAxis < 0)
+        {
+
+        }
+    }
+}
