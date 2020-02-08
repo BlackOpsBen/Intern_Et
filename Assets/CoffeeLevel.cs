660da3a8b06992c8a464b7cd4f46cd20062f7724
Ben Mora <blackopsben@gmail.com>
2020-02-07 18:06:07 -0600

    Employee's now get angry and have a countdown to loss state. (Need to implement loss state still)

---
+++ Assets/CoffeeLevel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeLevel : MonoBehaviour
+{
+    [SerializeField] static float maxCoffeeLevel = 10f;
+    public float coffeeLevel = maxCoffeeLevel;
+    [SerializeField] float lossRate = 1f;
+    [SerializeField] float lossAmount = 1f;
+    [SerializeField] float angerThreshold = 0.3f;
+    [SerializeField] Animator animator;
+
+    private void Start()
+    {
+        StartCoroutine(loseCoffee());
+    }
+
+    private IEnumerator loseCoffee()
+    {
+        while (coffeeLevel > float.Epsilon)
+        {
+            coffeeLevel -= lossAmount;
+            print(coffeeLevel); // TODO remove
+            if (coffeeLevel < maxCoffeeLevel * angerThreshold)
+            {
+                animator.SetBool("isAngry", true);
+            }
+            yield return new WaitForSeconds(lossRate);
+        }
+        print("Out of coffee! Game over!"); // TODO remove
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<CoffeeFly>())
+        {
+            coffeeLevel = maxCoffeeLevel;
+            animator.SetBool("isAngry", false);
+            animator.SetTrigger("Hit");
+        }
+    }
+}
