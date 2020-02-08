b53f37332a188d8d9d0f54e175c7198ebdb6d36c
Ben Mora <blackopsben@gmail.com>
2020-02-08 10:19:29 -0600

    Ready to try stopping wall movement

--- Assets/EmployeeWalk.cs
+++ Assets/EmployeeWalk.cs
@@ -76,4 +76,15 @@ public class EmployeeWalk : MonoBehaviour
         animator.SetBool("isWalking", false);
         direction = 0;
     }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "MoveStop")
+        {
+            print("Emplyee hit wall!");
+            direction = direction * -1;
+            isFacingLeft = !isFacingLeft;
+        }
+    }
+
 }
