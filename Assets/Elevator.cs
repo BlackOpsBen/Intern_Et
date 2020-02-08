6c22325ca8a47c514fd03c99b950638d0e0b585c
Ben Mora <blackopsben@gmail.com>
2020-02-08 17:15:35 -0600

    Elevators display correct arrows!

--- Assets/Elevator.cs
+++ Assets/Elevator.cs
@@ -9,4 +9,26 @@ public class Elevator : MonoBehaviour
     public GameObject door;
     public GameObject entryPoint;
     public GameObject insidePoint;
+    [SerializeField] SpriteRenderer spriteRenderer;
+
+    [ExecuteInEditMode]
+    private void Start()
+    {
+        if (!goingDown && !goingUp)
+        {
+            spriteRenderer.sprite = Resources.Load<Sprite>("Elevator_Base_None");
+        }
+        else if (goingDown && goingUp)
+        {
+            spriteRenderer.sprite = Resources.Load<Sprite>("Elevator_Base");
+        }
+        else if (goingDown && !goingUp)
+        {
+            spriteRenderer.sprite = Resources.Load<Sprite>("Elevator_Base_Down");
+        }
+        else if (!goingDown && goingUp)
+        {
+            spriteRenderer.sprite = Resources.Load<Sprite>("Elevator_Base_Up");
+        }
+    }
 }
